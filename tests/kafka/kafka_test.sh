256ee78599613e5e581b3906ea60f425e373f6a4
pchhum <pchhum@tetrascience.com>
2017-11-11 17:45:59 -0500

    use wait-for for travis build

--- tests/kafka/kafka_test.sh
+++ tests/kafka/kafka_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #create a kafka topic 'foobar'
-sleep 30
-kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
+./wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
