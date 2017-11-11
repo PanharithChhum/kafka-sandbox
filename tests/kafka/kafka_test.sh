6391b55cf1a49dd854abb33986ea70564bd6a40a
pchhum <pchhum@tetrascience.com>
2017-11-11 18:03:56 -0500

    test commit

--- tests/kafka/kafka_test.sh
+++ tests/kafka/kafka_test.sh
@@ -3,5 +3,4 @@
 # create a kafka topic 'foobar'
 # sleep 30
 #FIX THIS SO IT CAN RUN FROM ANYWHERE
-ls
 ./wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
