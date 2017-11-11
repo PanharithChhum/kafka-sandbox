a0aa399c79a961e49295cd609dfcab4befe90d49
pchhum <pchhum@tetrascience.com>
2017-11-11 17:52:01 -0500

    actually fixed directory

--- tests/kafka/kafka_test.sh
+++ tests/kafka/kafka_test.sh
@@ -2,4 +2,5 @@
 
 # create a kafka topic 'foobar'
 # sleep 30
-./tests/wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
+#FIX THIS SO IT CAN RUN FROM ANYWHERE
+../wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
