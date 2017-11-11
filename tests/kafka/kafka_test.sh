30db9a3f4fdc87c201751f17d0522311e8c2b3f9
pchhum <pchhum@tetrascience.com>
2017-11-11 18:00:50 -0500

    added wait-for script

--- tests/kafka/kafka_test.sh
+++ tests/kafka/kafka_test.sh
@@ -3,4 +3,5 @@
 # create a kafka topic 'foobar'
 # sleep 30
 #FIX THIS SO IT CAN RUN FROM ANYWHERE
-../wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
+ls
+./wait-for kafka:9092 -- kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor 1 --partitions 1 --topic foobar
